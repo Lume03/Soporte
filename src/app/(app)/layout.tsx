c17ec11bc9bda4e55d9125e9130f960ff5e0c892
Lume03 <jorge.luquem@unmsm.edu.pe>
2025-10-10 20:23:20 -0500

    elimine (app)

--- src/app/(app)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatLayout } from '@/components/chat/chat-layout';
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <ChatLayout>{children}</ChatLayout>;
-}
